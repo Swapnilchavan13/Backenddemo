b8d9faf8d172074d5a7d8313033b3e3607caecac
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-04-12 16:18:43 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Book = require('./models/books')
+const Book = require('./models/books');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,16 +11,19 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.log(error);
+        process.exit(1);
     }
-}
+};
+
+// parse application/json
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({title: 'Books'})
-})
+    res.send({title: 'Books'});
+});
 
 app.get('/add', async (req, res) => {
     try {
@@ -33,23 +36,23 @@ app.get('/add', async (req, res) => {
                 title:'New Is Awesome',
                 body:"something Awesome is here"
             },
-        ])
+        ]);
 
-        res.send('Books Added...')
+        res.send('Books Added...');
     } catch (error) {
-        console.log("Err" , + error)
+        console.log("Err", + error);
     }
-})
+});
 
-app.get('/data', async (req,res)=>{
-    const book= await Book.find();
+app.get('/data', async (req,res) => {
+    const book = await Book.find();
 
-    if(book){
-        res.json(book)
-    }else{
-        res.send("Something Went wrong.")
+    if (book) {
+        res.json(book);
+    } else {
+        res.send("Something Went wrong.");
     }
-})
+});
 
 // Route to get book by id
 app.get('/data/:id', async (req, res) => {
@@ -60,13 +63,28 @@ app.get('/data/:id', async (req, res) => {
         }
         res.json(book);
     } catch (error) {
-        console.log("Err", + error)
+        console.log("Err", + error);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Route to add a new book
+app.post('/data', async (req, res) => {
+    try {
+        const book = new Book({
+            title: req.body.title,
+            body: req.body.body
+        });
+        await book.save();
+        res.json(book);
+    } catch (error) {
+        console.log("Err", + error);
         res.status(500).send('Server Error');
     }
 });
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-    })
-})
+        console.log(`Listening on port ${PORT}`);
+    });
+});
