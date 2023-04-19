1b61336d67bb06a0bd24f379d95126a6785ef4d7
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-04-19 22:31:25 +0530

    update

---
+++ models/books.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const Bookschema = new Schema({
+    title:{
+        type: String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+    discription:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    }
+})
+module.exports = mongoose.model("Book", Bookschema)
