c69eca3f92151105a5b13cbd3b48feaf624ea234
swapnil chavan <chavanswapnil822@gmail.com>
2023-08-16 11:46:42 +0530

    updated

--- models/new.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const ImageSchema = new Schema({
-  filename: String,
-  contentType: String,
-});
-
-module.exports = mongoose.model('New', ImageSchema);
