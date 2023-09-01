af6a9b5f94e93884bf7152d006ef9593e64ebd1c
swapnil chavan <chavanswapnil822@gmail.com>
2023-09-01 14:07:52 +0530

    update

--- models/campaign.js
+++ models/campaign.js
@@ -1,42 +1,43 @@
 const mongoose = require('mongoose');
 
 const campaignSchema = new mongoose.Schema({
-  selectedVillages: {
-    type: [String],
-  },
-  selectedTalukas: {
-    type: [String],
-  },
-  selectedRanges: {
-    type: [String],
-  },
-  selectedGenders: {
-    type: [String],
-  },
-  selectedDistrict: {
-    type: String,
-  },
-  selectedAttributes: {
+      selectedVillages: {
+        type: [String],
+      },
+      selectedTalukas: {
+        type: [String],
+      },
+    selectedRanges: {
+        type: [String],
+      },
+      selectedGenders: {
+        type: [String],
+      },
+      selectedDistrict: {
+        type:String,
+      },
+selectedAttributes: {
     type: [String],
   },
   range: {
     type: String,
   },
   mediaVideo: {
-    type: Boolean, // Corrected data type to Boolean
+    type:String,
   },
   mediaSlide: {
-    type: Boolean, // Corrected data type to Boolean
+    type:String,
   },
   media3D: {
-    type: Boolean, // Corrected data type to Boolean
-  },
-  detail: {
     type: String,
   },
-  budget: {
-    type: Number, // Corrected data type to Number
+  detail: {
+    type:String,
   },
+
+  budget: {
+    type: String,
+}
 });
 
 module.exports = mongoose.model('Campaign', campaignSchema);
