8396c92452e3049d6986c08cf5e6d04555ea59d1
swapnil chavan <chavanswapnil822@gmail.com>
2023-09-14 13:32:03 +0530

    update

--- models/imgandvid.js
+++ models/imgandvid.js
@@ -2,16 +2,8 @@ const mongoose = require('mongoose');
 
 const subSchema = new mongoose.Schema({
     mainid: String,
-    imgurl1: String,
-    imgurl2: String,
-    imgurl3: String,
-    imgurl4: String,
-    imgurl5: String,
-    vidurl1: String,
-    vidurl2: String,
-    vidurl3: String,
-    vidurl4: String,
-    vidurl5: String,
+    imgurl1: [String],
+    vidurl1: [String],
   });
 
 const imgandvidSchema = new mongoose.Schema({
