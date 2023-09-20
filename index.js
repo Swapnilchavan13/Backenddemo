2289ee052eba6c85b2f49c8b41936f7471611b52
swapnil chavan <chavanswapnil822@gmail.com>
2023-09-20 10:31:42 +0530

    updated Id

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/address', async (req, res) => {
 // Get an address by ID
 app.get('/address/:id', async (req, res) => {
   try {
-    const address = await Address.findById(req.params._id);
+    const address = await Address.findById(req.params.id);
     if (!address) {
       return res.status(404).json({ error: 'Address not found' });
     }
@@ -88,6 +88,7 @@ app.get('/address/:id', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
+
 
 // Update an address without specifying an ID
 app.put('/address', async (req, res) => {
